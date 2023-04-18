c3c09b059ce7b7898f0b4b464052d0524691a741
Aidana777 <aidananiazalieva27@gmail.com>
2023-04-18 17:47:40 +0600

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import BookList from "./components/BookList/BookList";
 import CardItem from "./components/BookList/CardItem";
+import Navbar from './pages/Navbar'
 
 
 
@@ -9,6 +10,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path="/booklist" element={<BookList />} />
         <Route path="/carditem" element={<CardItem />} />
