0dd2ed6caa949312a2ca28cebe9333f8dc359a7c
Aidana777 <aidananiazalieva27@gmail.com>
2023-08-28 19:07:34 +0600

    Sort

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,24 +1,67 @@
 import React, { useState } from "react";
-import data from '../../db.json'
-import Card from '../Card/Card';
-import SearchBar from '../SearchBar/SearchBar';
+import data from "../../db.json";
+import Card from "../Card/Card";
+import SearchBar from "../SearchBar/SearchBar";
+import Sidebar from "../Sidebar/Sidebar";
 
 const BookList = () => {
   const [searchResults, setSearchResults] = useState(data);
+  const [sortAsc, setSortAsc] = useState(true);
+  const [showOnlyAvailable, setShowOnlyAvailable] = useState(false);
 
   const handleSearch = (searchText) => {
-    const results = data.filter(card => card.name.toLowerCase().includes(searchText.toLowerCase()));
+    const results = data.filter((card) =>
+      card.name.toLowerCase().includes(searchText.toLowerCase())
+    );
     setSearchResults(results);
   };
 
+  const handleSortChange = () => {
+    setSortAsc(!sortAsc);
+    const sortedResults = [...searchResults].sort((a, b) => {
+      if (sortAsc) {
+        return a.price - b.price;
+      } else {
+        return b.price - a.price;
+      }
+    });
+    setSearchResults(sortedResults);
+  };
+  const handleCheckboxChange = () => {
+    setShowOnlyAvailable(!showOnlyAvailable);
+  
+    if (!showOnlyAvailable) {
+      const availableResults = searchResults.filter((card) => card.available);
+      setSearchResults(availableResults);
+    } else {
+      setSearchResults(data);
+    }
+  };
+  const handleReset = () => {
+    setSearchResults(data);
+    setSortAsc(true);
+    setShowOnlyAvailable(false);
+  };
+
   return (
-    <div>
-      <SearchBar onSearch={handleSearch} />
-      <div className="grid grid-cols-3 gap-20">
-        {searchResults.map(card => <Card key={card.id} card={card} />)}
+    <div className="flex">
+      <Sidebar
+        products={data}
+        onSortChange={handleSortChange}
+        onCheckboxChange={handleCheckboxChange}
+        onReset={handleReset}
+        sortAsc={sortAsc}
+      />
+      <div>
+        <SearchBar onSearch={handleSearch} />
+        <div className="grid grid-cols-3 gap-20">
+          {searchResults.map((card) => (
+            <Card key={card.id} card={card} />
+          ))}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default BookList;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Sidebar = ({ products }) => {
+const Sidebar = ({ products, onSortChange, onCheckboxChange, onReset, sortAsc }) => {
   return (
     <div className="w-1/4 bg-gray-200 p-4">
-      <h2 className="text-xl font-semibold mb-4">All Products</h2>
+      <h2 className="text-xl font-semibold mb-4">Products and Filters</h2>
       <ul className="space-y-2">
-        {products.map((product) => (
+        {products && products.map((product) => ( 
           <li key={product.id}>
             <Link to={`/booklist/${product.id}`} className="text-blue-500 hover:underline">
               {product.name}
@@ -14,14 +14,36 @@ const Sidebar = ({ products }) => {
           </li>
         ))}
       </ul>
-      <Link
-        to="/cart"
-        className="mt-4 block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-      >
-        Go to Cart
-      </Link>
+      <div className="mb-4">
+        <label className="block mb-2">
+          <input
+            type="checkbox"
+            onChange={onCheckboxChange}
+            className="mr-2"
+          />
+          Show Only Available
+        </label>
+      </div>
+      <div className="mb-4">
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          onClick={onSortChange}
+        >
+          {sortAsc ? "Sort Descending" : "Sort Ascending"}
+        </button>
+      </div>
+      <div>
+        <button
+          className="mt-4 block bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+          onClick={onReset}
+        >
+          Reset Filters
+        </button>
+      </div>
+ 
     </div>
   );
 };
 
 export default Sidebar;
+
